c41a85b390842374861a7d7a155de1ad09e00185
Bret Comnes <bcomnes@gmail.com>
2019-11-07 16:54:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const exec = require('child_process').exec
 
-exec('sh cleanup-xvfb.sh', (error, stdout, stderr) => {
+exec('sh ./cleanup-xvfb.sh', (error, stdout, stderr) => {
   console.log(stdout)
   console.log(stderr)
   if (error !== null) {
